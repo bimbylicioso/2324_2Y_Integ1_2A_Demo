e76e53ef2138f7a7cf98f6b8e688c24272fac19b
22-0004c <22-0004c@CCL01-33>
2024-02-19 17:16:41 +0800

    INTRO

--- 2324_2Y_Integ1_2A_Demo/MainWindow.xaml.cs
+++ 2324_2Y_Integ1_2A_Demo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,7 @@ namespace _2324_2Y_Integ1_2A_Demo
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable dataTable = new DataTable();
         Button[] btnNums = new Button[10];
         int num1 = 0;
         int num2 = 0;
@@ -49,81 +51,85 @@ namespace _2324_2Y_Integ1_2A_Demo
             btnMult.Content = "x";
             btnDiv.Content = "/";
             btnEnter.Content = "=";
+            this.KeyDown += Window_KeyDown;
         }
 
-        private void numberEnter(int x)
-        {
-            string input = tbCalc.Text;
-            input += x;
 
-            if(input.Length > 5 )
-                input = input.Substring(1);
+        private void numberEnter(string input)
+        {
+            string currentText = tbCalc.Text;
+
+            if (currentText.Length > 12)
+                currentText = currentText.Substring(1);
+
+            if (string.IsNullOrEmpty(currentText))
+                currentText = "0";
 
             if (ope == -1)
-                num1 = int.Parse(input);
+                num1 = int.Parse(currentText + input);
             else
-                num2 = int.Parse(input);
+                num2 = int.Parse(currentText + input);
 
-            tbCalc.Text = input;
+            tbCalc.Text += input;
         }
 
         #region KeypadEvents
         private void btn9_Click(object sender, RoutedEventArgs e)
         {
-            numberEnter(9);
+            numberEnter("9");
         }
 
         private void btn8_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(8);
+            numberEnter("8");
         }
 
         private void btn7_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(7);
+            numberEnter("7");
         }
 
         private void btn6_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(6);
+            numberEnter("6");
         }
 
         private void btn5_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(5);
+            numberEnter("5");
         }
 
         private void btn4_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(4);
+            numberEnter("4");
         }
 
         private void btn3_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(3);
+            numberEnter("3");
         }
 
         private void btn2_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(2);
+            numberEnter("2");
         }
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
 
-            numberEnter(1);
+            numberEnter("1");
         }
 
         private void btn0_Click(object sender, RoutedEventArgs e)
         {
-            numberEnter(0);
+            numberEnter("0");
 
         }
         #endregion
@@ -151,32 +157,79 @@ namespace _2324_2Y_Integ1_2A_Demo
         {
             ope = 3;
             tbCalc.Text = "";
-        } 
+        }
         #endregion
 
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
-            switch(ope)
+            try
             {
-                case 0:
-                    num1 += num2;
-                    break;
-                case 1:
-                    num1 -= num2;
-                    break;
-                case 2:
-                    num1 *= num2;
-                    break;
-                case 3:
-                    num1 /= num2;
-                    break;
+                // Evaluate the expression using DataTable.Compute
+                var result = dataTable.Compute(tbCalc.Text, "");
+                tbCalc.Text = result.ToString();
             }
-            
-            if(ope > -1)
+            catch (Exception ex)
             {
-                tbCalc.Text = num1.ToString();
-                ope = -1;
-                num2 = 0;
+                // Handle the case where the expression is invalid
+                tbCalc.Text = "Error";
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            tbCalc.Text = "";
+        }
+
+
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            if (!tbCalc.Text.Contains("."))
+            {
+                tbCalc.Text += ".";
+            }
+        }
+
+        private void btnBackspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbCalc.Text.Length > 0)
+            {
+                tbCalc.Text = tbCalc.Text.Substring(0, tbCalc.Text.Length - 1);
+            }
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                int number = (int)(e.Key - Key.D0);
+                numberEnter(number.ToString());
+            }
+            else if (e.Key == Key.Decimal)
+            {
+                btnDot_Click(sender, e);
+            }
+            else if (e.Key == Key.Add)
+            {
+                btnAdd_Click(sender, e);
+            }
+            else if (e.Key == Key.Subtract)
+            {
+                btnMin_Click(sender, e);
+            }
+            else if (e.Key == Key.Multiply)
+            {
+                btnMult_Click(sender, e);
+            }
+            else if (e.Key == Key.Divide)
+            {
+                btnDiv_Click(sender, e);
+            }
+            else if (e.Key == Key.Enter)
+            {
+                btnEnter_Click(sender, e);
+            }
+            else if (e.Key == Key.Back)
+            {
+                btnBackspace_Click(sender, e);
             }
         }
     }
